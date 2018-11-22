1ae4e9cfedbc6d214ca2987cc57eeb16f640b49b
msheikhalard <Miky_Mike123>
2018-11-22 16:16:48 +0100

    Classes for hotel

---
+++ src/hotel/Booking.java
@@ -0,0 +1,42 @@
+package hotel;
+
+public class Booking {
+    private String checkIn;
+    private String checkOut;
+    private Room room;
+    private Guest guest;
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
+
+    public String getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(String checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public String getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(String checkOut) {
+        this.checkOut = checkOut;
+    }
+
+
+}
---
+++ src/hotel/RoomType.java
@@ -0,0 +1,31 @@
+package hotel;
+
+public class RoomType {
+    private String type;
+    private double price;
+    private int size;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
