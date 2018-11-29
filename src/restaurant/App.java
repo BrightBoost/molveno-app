967bb7d00cf47881c5b453677ec12ebb6a6dca18
yrahim <yama.rahim@capgemini.com>
2018-11-29 14:30:10 +0100

    molveno app resturant

--- src/restaurant/App.java
+++ src/restaurant/App.java
@@ -9,27 +9,29 @@ public class  App {
         Guest geust1 = new Guest();
         Table table1 = new Table() ;
         Scanner sc = new Scanner(System.in);
-        System.out.println("What is your name?");
+        System.out.println("Enter your name:");
         String x = sc.nextLine();
          geust1.setName(x);
-        System.out.println("The name is " + geust1.getName());
+        //System.out.println("The name is " + geust1.getName());
         Scanner sc1 = new Scanner(System.in);
-        System.out.println("What is your telephone number ?");
+        System.out.println("Enter your phone number:");
         String y = sc.nextLine();
         geust1.setMobileNumber(y);
-        System.out.println("Your telephone number is  "+ geust1.getMobileNumber());
+        //System.out.println("Your telephone number is  "+ geust1.getMobileNumber());
         Scanner sc2 = new Scanner (System.in);
-        System.out.println ("For how many persons?");
+        System.out.println ("Number of persons:");
         int z = sc2.nextInt();
         table1.setNumberOfChairs(z);
-        System.out.println("Number of persons are " + table1.getNumberOfChairs());
+        //System.out.println("Number of persons are " + table1.getNumberOfChairs());
         reservation1.setGuest(geust1);
         reservation1.setTable(table1);
         Scanner sc3 = new Scanner(System.in);
-        System.out.println("What time do you prefer ?");
+        System.out.println("Enter date and time:");
         String t = sc3.nextLine();
         reservation1.setReservationTime(t);
-        System.out.println("We have made a reservation for " + reservation1.getTable().getNumberOfChairs()+ " presons at " + reservation1.getReservationTime());
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("We have made a reservation for you: " + "\nName:    " +reservation1.getGuest().getName() + "\nPhone:  " +reservation1.getGuest().getMobileNumber());
+        System.out.println("Number of persons:  " + reservation1.getTable().getNumberOfChairs()+ "\nDate and Time:    " + reservation1.getReservationTime());
 
 
 
